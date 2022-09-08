8aed42081f4f16689925e102f5fd593cedb5df8e
chitraksh <chitrakshsingh007@gmail.com>
2022-09-09 01:08:39 +0530

    initial commit

---
+++ bme280_test/bme280_test.ino
@@ -0,0 +1,50 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+byte degree[8] =
+{
+  0b00011,
+  0b00011,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000
+};
+
+Adafruit_BME280 bme;
+
+void setup() {
+  Serial.begin(9600);
+
+  if (!bme.begin(0x77)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+}
+
+void loop() {
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println("*C");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println("%");
+
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println("hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println("m");
+
+
+
+  Serial.println();
+  delay(1000);
+}
---
+++ esp8266_conn/esp8266_conn.ino
@@ -0,0 +1,42 @@
+//#include <ESP8266WiFi.h>
+//
+//void setup(){
+//  Serial.begin(9600);
+//  WiFi.begin("Dev", "9619177458");
+//  while(WiFi.status() != WL_CONNECTED){
+//    Serial.print("..");
+//    delay(200);
+//  }
+//  Serial.println();
+//  Serial.println("NodeMCU is COnnected");
+//  Serial.println(WiFi.localIP());
+//}
+//
+//void loop(){
+//  
+//}
+
+/*
+  ESP8266 Blink by Simon Peter
+  Blink the blue LED on the ESP-01 module
+  This example code is in the public domain
+
+  The blue LED on the ESP-01 module is connected to GPIO1
+  (which is also the TXD pin; so we cannot use Serial.print() at the same time)
+
+  Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
+*/
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  // but actually the LED is on; this is because
+  // it is active low on the ESP-01)
+  delay(1000);                      // Wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+}
